3642e25ba1b94973724e03bcce7becb297fd3176
davidlarrondo <81131837+davidlarrondo@users.noreply.github.com>
2026-06-09 16:13:18 -0400

    primera carga de archivos
    se agregar funciones sumar y restar

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+# Función para sumar dos números
+def sumar(a, b):
+    return a + b
+
+# Función para restar dos números
+def restar(a, b):
+    return a - b
+
+# Ejemplos de uso
+resultado_suma = sumar(5, 3)   # 5 + 3 = 8
+resultado_resta = restar(5, 3) # 5 - 3 = 2
+
+print("Suma:", resultado_suma)
+print("Resta:", resultado_resta)
