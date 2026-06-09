73791bf050c9f0184064ad2ebadbd8bb787cdb69
davidlarrondo <81131837+davidlarrondo@users.noreply.github.com>
2026-06-09 16:19:35 -0400

    incorporar función multiplicar
    incorporar función multiplicar con retorno

--- calculadora.py
+++ calculadora.py
@@ -6,9 +6,15 @@ def sumar(a, b):
 def restar(a, b):
     return a - b
 
+# Función para multiplicar dos números
+def multiplicar(a, b):
+    return a * b
+
 # Ejemplos de uso
 resultado_suma = sumar(5, 3)   # 5 + 3 = 8
 resultado_resta = restar(5, 3) # 5 - 3 = 2
+resultado_multiplicar = multiplicar(5, 3) # 5 * 3 = 15
 
 print("Suma:", resultado_suma)
 print("Resta:", resultado_resta)
+print("Multiplicar:", resultado_multiplicar)
